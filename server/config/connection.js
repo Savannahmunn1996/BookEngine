1004fc6d949ec47c61868573dc92554d2feec802
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-04-21 19:32:15 -0700

    connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://savannahmunn1996:Milo2023@cluster0.lrnkxk9.mongodb.net/test/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
